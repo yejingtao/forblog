5b1f3ff6888cab30e5670750e58e725e2128535a
yejingtao <250234464@qq.com>
2017-09-18 15:57:59 +0800

    3-4

---
+++ demo3-config-server/src/main/java/com/Application.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableConfigServer
+@SpringBootApplication
+@EnableEurekaClient
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
